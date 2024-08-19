3933c1fc9067f0d210933b8b4e1ce4089b89443a
DANIL0372 <danon45root@gmail.com>
2024-08-19 16:13:08 +0300

    init

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -9,6 +9,23 @@ const Form = () => {
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            city,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, []);
+
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
