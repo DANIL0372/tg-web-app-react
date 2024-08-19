37bbc8b49bd22471e416fcf7105a9552db466131
DANIL0372 <danon45root@gmail.com>
2024-08-19 20:10:25 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import header from "./components/header/header"
 import {Route, Routes} from 'react-router-dom'
-import Produdctlist from "./components/productlist/productlist";
+import Productlist from "./components/productlist/productlist";
 import Form from "./components/form/form"
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <div className="App">
             <header />
             <Routes>
-                <Route index element={<Produdctlist/>}/>
+                <Route index element={<Productlist/>}/>
                 <Route path={'form'} element={<Form />}/>
                 {/*<Route path={'productlist'} element={<Produdctlist />}/>*/}
             </Routes>
